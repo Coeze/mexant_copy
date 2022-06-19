6ed1f2c478705452187350d3f5dd6522913ec9d1
Daniel <coeze04@gmail.com>
2022-06-19 12:51:46 +0100

    added carousel

--- index.js
+++ index.js
@@ -4,35 +4,65 @@ const webCoding = document.querySelector("div.web_coding")
 const webDesignButton = document.querySelector("a#wd")
 const webCodingButton = document.querySelector("a#wc")
 const GraphicsButton = document.querySelector("a#gr")
+const forwardArrow = document.querySelector("div.forward_arrow")
+const backArrow = document.querySelector("div.back_arrow")
+const Header = document.querySelector(".index_header")
 
+
+const urls = ["http://127.0.0.1:5500/media/images/main_background.jpg", 'http://127.0.0.1:5500/media/images/slide2.jpg', 'http://127.0.0.1:5500/media/images/slide3.jpg']
+
+forwardArrow.addEventListener("click", () => { 
+const style = window.getComputedStyle(Header, false)  
+const url = style.backgroundImage.slice(4, -1).replace(/"/g, "");  
+const currIdx = urls.indexOf(url)
+const newIdx = (currIdx + 1) % urls.length
+Header.style.backgroundImage = `url(${urls[newIdx]})`
+Header.style.transition = "all 1s";
+
+})
+
+backArrow.addEventListener("click", () => { 
+    const style = window.getComputedStyle(Header, false)  
+    const url = style.backgroundImage.slice(4, -1).replace(/"/g, "");  
+    const currIdx = urls.indexOf(url)
+    const newIdx = Math.abs((currIdx - 1) % urls.length)
+    Header.style.backgroundImage = `url(${urls[newIdx]})`
+    
+    })
 
 GraphicsButton.addEventListener("click", () => {
-    if (webDesign.style.display == "block"){
-        webDesign.classList.add(".hide")
+    if (!(webDesign.classList.contains("hide"))){
+        webDesign.classList.add("hide")
     }
-
-    if (webCoding.style.display == "block"){
-        webCoding.classList.add(".hide")
+    if (!(webCoding.classList.contains("hide"))){
+        webCoding.classList.add("hide")
     }
-    graphics.classList.add(".show")
+    if (graphics.classList.contains("hide")){
+        graphics.classList.remove("hide")
+    }
+    
 })
 webDesignButton.addEventListener("click", () => {
-    webDesign.classList.add(".show")
-    if (webCoding.style.display == "block"){
-        webCoding.classList.add(".hide")
+    if (!(graphics.classList.contains("hide"))){
+        graphics.classList.add("hide")
     }
-    if (graphics.style.display == "block"){
-    graphics.classList.add(".hide")
+    if (!(webCoding.classList.contains("hide"))){
+        webCoding.classList.add("hide")
+    }
+    if (webDesign.classList.contains("hide")){
+        webDesign.classList.remove("hide")
     }
 })
 webCodingButton.addEventListener("click", () => {
-    if (webDesign.style.display == "block"){
-        webDesign.classList.add(".hide")
+    if (!(webDesign.classList.contains("hide"))){
+        webDesign.classList.add("hide")
+    }
+    if (!(graphics.classList.contains("hide"))){
+        graphics.classList.add("hide")
+    }
+    if (webCoding.classList.contains("hide")){
+        webCoding.classList.remove("hide")
     }
 
-    webCoding.classList.add(".show")
-    if (graphics.style.display == "block"){
-        graphics.classList.add(".hide")
-        }
-
 })
+
